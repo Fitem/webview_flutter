e8bb42962c2e9201a528ff9418f0f76d20a6e0b1
Fitem <Fitem@users.noreply.github.com>
2021-11-02 18:37:51 +0800

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -57,7 +57,7 @@ class _WebViewExampleState extends State<WebViewExample> {
       // to allow calling Scaffold.of(context) so we can show a snackbar.
       body: Builder(builder: (BuildContext context) {
         return WebView(
-          initialUrl: 'https://123.6.38.196:9999/h5/#/playRuleEntry',
+          initialUrl: 'xxx',
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController webViewController) {
             _controller.complete(webViewController);
